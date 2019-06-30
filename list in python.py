c1770b51cf93a48fccaea509a34025182f2e9565
Bhartiramteke123cognitiors <52369659+Bhartiramteke123cognitiors@users.noreply.github.com>
2019-06-30 13:43:47 +0530

    Add files via upload

---
+++ list in python.py
@@ -0,0 +1,30 @@
+my_first_list = [10,20,30,40,50,60,70,80,90,100]
+my_first_list
+
+nested_list = [['kat',12],['jack',26],['hugo',43]]
+nested_list
+
+type(my_first_list)
+
+type(nested_list)
+
+my_first_list[5]
+
+my_first_list[-1]
+
+my_first_list[-5]
+
+my_first_list[-5::-1]
+
+my_first_list[1:8:2]
+
+my_first_list.append(120)
+my_first_list
+
+my_first_list_1=my_first_list.copy()
+my_first_list_1
+
+my_first_list_1.clear()
+my_first_list_1
+my_first_list.reverse()
+my_first_list
