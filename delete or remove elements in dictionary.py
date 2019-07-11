e62100deceb411646c21bbd55a9bcdc6ba5ae5e4
Bhartiramteke123cognitiors <52369659+Bhartiramteke123cognitiors@users.noreply.github.com>
2019-07-11 10:52:02 +0530

    Add files via upload

---
+++ delete or remove elements in dictionary.py
@@ -0,0 +1,20 @@
+squares = {1:2, 2:3, 3:9, 4:20, 5:65}  
+
+print(squares.pop(3))  
+
+print(squares)
+
+print(squares.popitem())
+
+print(squares)
+
+del squares[4]  
+
+print(squares)
+
+squares.clear()
+
+print(squares)
+
+del squares
+
---
+++ tupple.py
@@ -0,0 +1,13 @@
+my_first_tupple = (10,20,30,40,50,60,70,80,90,100)
+my_first_tupple
+tupple
+
+my_first_list = (10,20,30,40,50)
+my_first_list
+
+my_first_tupple[5]
+len(my_first_tupple)
+
+my_first_tupple.index(60)
+my_first_tupple.count(60)
+
